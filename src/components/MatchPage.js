ff6ccc283a35b887fbb2a0cdefc999a7933b9f91
ryjtoh <ryjtoh@gmail.com>
2021-01-23 18:13:19 -0800

    ds

---
+++ src/components/MatchPage.js
@@ -0,0 +1,19 @@
+import '../App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useParams
+  } from "react-router-dom";
+
+
+function MatchPage() {
+    return (
+        <div>
+            Hello FELLAHSHIP!!
+        </div>
+    );
+}
+
+export default MatchPage;
